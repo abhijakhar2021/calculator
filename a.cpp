2aea1607451f83bc30a71ea9389c0e3960cdbdc3
Abhishek Jakhar <jakharabhishek028@gmail.com>
2023-09-19 02:59:13 +0530

    calculator

---
+++ a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1 ; i<=n ; i++) 
+    {
+        int sum = 0;
+        sum=sum+i;
+        cout<<sum;
+    }
+    
+
+    
+
+    return 0;
+}
